49541980bb78002af84ee9155bdcfb20893929d2
Emre Zeytin <emrezeytin@emrezeytin.local>
2023-01-02 23:47:33 +0300

    add mantine

---
+++ TODO.md
@@ -0,0 +1,24 @@
+- mantine for ui
+- apollo for data
+- express for server
+- mongodb for database
+
+
+- user signup and login with their emails or social auth
+- user can add characters, places, episodes to favorite list
+- user can create their own lists
+- user can make comments on characters, places, episodes
+- user can select episodes to "watched"
+
+
+- index
+- characters
+- locations
+- episodes
+- character/[character]
+- location/[location]
+- episode/[episode]
+- dashboard
+- lists
+- login
+- signup
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,72 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react";
+import {
+	AppShell,
+	Navbar,
+	Header,
+	Text,
+	MediaQuery,
+	Burger,
+	useMantineTheme,
+	MantineProvider,
+} from "@mantine/core";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const theme = useMantineTheme();
+	const [opened, setOpened] = useState(false);
+	return (
+		<MantineProvider
+			withGlobalStyles
+			withNormalizeCSS
+			theme={{ colorScheme: "dark" }}
+		>
+			<AppShell
+				styles={{
+					main: {
+						background:
+							theme.colorScheme === "dark"
+								? theme.colors.dark[8]
+								: theme.colors.gray[0],
+					},
+				}}
+				navbarOffsetBreakpoint="sm"
+				navbar={
+					<Navbar
+						p="md"
+						hiddenBreakpoint="sm"
+						hidden={!opened}
+						width={{ sm: 200, lg: 300 }}
+					>
+						<Text>Application navbar</Text>
+					</Navbar>
+				}
+				header={
+					<Header
+						height={{ base: 50, md: 70 }}
+						p="md"
+					>
+						<div
+							style={{ display: "flex", alignItems: "center", height: "100%" }}
+						>
+							<MediaQuery
+								largerThan="sm"
+								styles={{ display: "none" }}
+							>
+								<Burger
+									opened={opened}
+									onClick={() => setOpened((o) => !o)}
+									size="sm"
+									color={theme.colors.gray[6]}
+									mr="xl"
+								/>
+							</MediaQuery>
+							<Text>Application header</Text>
+						</div>
+					</Header>
+				}
+			>
+				<Component {...pageProps} />
+			</AppShell>
+		</MantineProvider>
+	);
 }
